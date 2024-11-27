9d316aa8a2bef3cc3ce2fc896472df16ae0b5b4c
auriga <arnaud.pissot.auriga@gmail.com>
2024-11-27 19:46:13 +0100

    Improved: More pleasant look on Contact page Added: CV with preview

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,15 +1,39 @@
 <template>
   <div class="about">
     <n-card title="Me contacter">
-      Je suis joignable sur:<br>
-      - <a href="https://www.linkedin.com/in/arnaud-pissot-a7512710b/" target="_blank">Linked In</a><br>
-      - Mon email: <a href="mailto:arnaud.pissot.auriga@gmail.com">arnaud.pissot.auriga@gmail.com</a>
+      <a href="https://www.linkedin.com/in/arnaud-pissot-a7512710b/" target="_blank"><LogoLinkedin style="height: 25px; margin-right: 10px;" /><span style="vertical-align: top;">LinkedIn</span></a><br>
+      <a href="mailto:arnaud.pissot.auriga@gmail.com"><Mail style="height: 25px; margin-right: 10px;" /><span style="vertical-align: top;">arnaud.pissot.auriga@gmail.com</span></a>
+    </n-card>
+    <n-card title="Mon CV" style="margin-top:10px;">
+            <a href="/files/CV_arnaud_pissot.pdf" target="_blank"><Download style="height: 30px; margin-right: 10px;" /><span style="vertical-align: 0.5em;">Télécharger</span></a><br>
+      <n-image
+        height="600"
+        src="/imgs/CV_arnaud_pissot.png"
+      />
     </n-card>
   </div>
 </template>
 
 <script>
+import { defineComponent } from "vue";
+import {
+  LogoLinkedin as LogoLinkedin,
+  Mail as Mail,
+  Download as Download,
+} from "@vicons/ionicons5";
+
+export default defineComponent({
+  components: {
+    LogoLinkedin,
+    Mail,
+    Download
+  }
+});
 </script>
 
 <style scoped>
+a {
+  color: #18a058;
+  text-decoration: none;
+}
 </style>
